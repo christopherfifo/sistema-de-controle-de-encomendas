538b057a3d06c78b73c3022fca9c99f611d77737
GabrielVitorGL <gabrielvitorcom@hotmail.com>
2026-06-26 02:58:04 -0300

    Provide separate maskable and transparent icons to fix Android splash screen and launcher simultaneously

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -19,10 +19,22 @@ export default function manifest(): MetadataRoute.Manifest {
         src: '/icon-192x192.png',
         sizes: '192x192',
         type: 'image/png',
-        purpose: 'maskable',
+        purpose: 'any',
       },
       {
         src: '/icon-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'any',
+      },
+      {
+        src: '/icon-maskable-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+        purpose: 'maskable',
+      },
+      {
+        src: '/icon-maskable-512x512.png',
         sizes: '512x512',
         type: 'image/png',
         purpose: 'maskable',
